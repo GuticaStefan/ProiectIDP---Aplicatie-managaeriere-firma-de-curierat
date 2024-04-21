d2b0754d7a4801b4dbc33603b3e94554f9305af1
Radu Nistorescu <radu.nistorescu@stud.acs.upb.ro>
2024-04-21 19:59:53 +0300

    Added user API

---
+++ CourierAPP/src/main/java/com/proiect/CourierAPP/resource/UserController.java
@@ -0,0 +1,65 @@
+package com.proiect.CourierAPP.resource;
+
+import com.proiect.CourierAPP.dtos.PasswordDto;
+import com.proiect.CourierAPP.dtos.UserDto;
+import com.proiect.CourierAPP.dtos.UserUpdateDto;
+import com.proiect.CourierAPP.service.UserService;
+import jakarta.validation.Valid;
+import jakarta.websocket.server.PathParam;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/users")
+public class UserController {
+    private final UserService userService;
+
+    @PatchMapping("/{userName}")
+    @ResponseStatus(HttpStatus.CREATED)
+    public UserDto updateUser(@PathVariable String userName, @RequestBody @Valid UserUpdateDto userUpdateDto) {
+        return userService.update(userName, userUpdateDto);
+    }
+
+    @PatchMapping("/{userName}/password")
+    @ResponseStatus(HttpStatus.OK)
+    public void changePassword(@PathVariable String userName, @RequestBody @Valid PasswordDto passwordDto) {
+        userService.changePassword(userName, passwordDto);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<UserDto> findAll() {
+        return userService.findAll();
+    }
+
+
+    @GetMapping("/{userName}")
+    @ResponseStatus(HttpStatus.OK)
+    public Optional<UserDto> findByUserName(@PathVariable("userName") String userName) {
+        return userService.findByUserName(userName);
+    }
+
+    @GetMapping("/firstName")
+    @ResponseStatus(HttpStatus.OK)
+    public List<UserDto> findByFirstName(@PathParam("firstName") String firstName) {
+        return userService.findByFirstName(firstName);
+    }
+
+    @GetMapping("/lastName")
+    @ResponseStatus(HttpStatus.OK)
+    public List<UserDto> findByLastName(@PathParam("lastName") String lastName) {
+        return userService.findByLastName(lastName);
+    }
+
+    @DeleteMapping("/{userName}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteById(@PathVariable("userName") String userName) {
+        userService.deleteByUserName(userName);
+    }
+
+}
